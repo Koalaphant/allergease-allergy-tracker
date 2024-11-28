658dd48273a2c766c9cc30686b2b161961ceb688
Koalaphant <andrew.wardjones@icloud.com>
2024-11-28 23:43:06 +0000

    setting up express server

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,27 @@
-function hello() {
-  console.log("test");
-}
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.json([
+    {
+      id: 1,
+      name: "Andrew",
+      isStudent: false,
+    },
+    {
+      id: 2,
+      name: "Grace",
+      isStudent: false,
+    },
+    {
+      id: 3,
+      name: "Chris",
+      isStudent: true,
+    },
+  ]);
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
